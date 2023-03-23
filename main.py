76b7e732e8a1e554842e772850cf41891efa6616
Taras <tarvar988@gmail.com>
2023-03-23 08:08:26 +0200

    Added sending email code with declaration of the user's email address

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 import selectorlib
+import smtplib, ssl
+import os
+
 
 URL = 'https://programmer100.pythonanywhere.com/tours/'
 HEADERS = {
@@ -13,13 +16,42 @@ def scrape(url):
     source = response.text
     return source
 
-
 def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file('extract.yaml')
     value = extractor.extract(source)['tours']
     return value
 
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "youremail@gmail.com"
+    password = "here_goes_your_gmail_password"
+
+    receiver = "youremail@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+    print('Email was sent!')
+
+def store(extracted):
+    with open('data.txt', 'a') as file:
+        file.write(extracted + '\n')
+
+def read(extracted):
+    with open('data.txt', 'r') as file:
+        return file.read()
+
+
 if __name__ == '__main__':
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
+
+    content = read(extracted)
+    if extracted != 'No upcoming tours':
+        if extracted not in content:
+            store(extracted)
+            send_email(message='Hey, new event was found!')
